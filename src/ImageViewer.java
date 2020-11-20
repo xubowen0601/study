680bb6faf91e921f78ed5045da7fc066766a1f3e
xubowen0601 <786875770@qq.com>
2020-11-20 21:21:49 +0800

    图形化打开图片界面

---
+++ src/ImageViewer.java
@@ -0,0 +1,48 @@
+import javax.swing.*;
+import java.awt.*;
+import java.io.File;
+
+public class ImageViewer {
+    public static void main(java.lang.String[] args) {
+        EventQueue.invokeLater(() -> {
+            JFrame frame = new ImageViewerFrame();
+            frame.setTitle("ImageViewer");
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setVisible(true);
+        });
+    }
+}
+
+class ImageViewerFrame extends JFrame {
+    private JLabel label;
+    private JFileChooser chooser;
+    private static final int DEFAULT_WIDTH = 300;
+    private static final int DEFAULT_HEIGHT = 400;
+
+    public ImageViewerFrame() {
+        setSize(DEFAULT_WIDTH, DEFAULT_HEIGHT);
+
+        label = new JLabel();
+        add(this.label);
+        chooser = new JFileChooser();
+        chooser.setCurrentDirectory(new File("."));
+        JMenuBar menuBar = new JMenuBar();
+        setJMenuBar(menuBar);
+        JMenu menu = new JMenu("File");
+        menuBar.add(menu);
+        JMenuItem openItem = new JMenuItem("Open");
+        menu.add(openItem);
+        openItem.addActionListener(event -> {
+            int result = chooser.showOpenDialog(null);
+            if (result == JFileChooser.APPROVE_OPTION) {
+                String name = this.chooser.getSelectedFile().getPath();
+                this.label.setIcon(new ImageIcon(name));
+            }
+        });
+        JMenuItem exitItem = new JMenuItem("Exit");
+        menu.add(exitItem);
+        exitItem.addActionListener(event -> System.exit(0));
+    }
+}
+
+ 
