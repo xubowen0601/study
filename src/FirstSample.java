825e8b0b295d6e2f5d077a358c4b78747913c7dc
xubowen0601 <786875770@qq.com>
2020-11-21 17:28:53 +0800

    声明变量常量，并输出

---
+++ src/FirstSample.java
@@ -0,0 +1,19 @@
+public class FirstSample {
+    public static void main(String[] args) {
+        System.out.println("We will not use 'Hello World!'");
+        System.out.println(2.0 - 1.1);
+        int vacation = 12;
+        System.out.println(vacation);
+        double salary = 65000.0;
+        System.out.println(salary);
+        final double CM_PER_INCH = 2.54;
+        double paperWidth = 8.5;
+        double paperHeight = 11;
+        System.out.println("Paper size in centimeters:"
+                + paperWidth * CM_PER_INCH + "by" + paperHeight * CM_PER_INCH);
+    }
+}
+/**
+ *
+ *
+ */
