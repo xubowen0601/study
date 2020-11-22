d632292bf8e926b8ec177350203e41bcb7bd159a
xubowen0601 <786875770@qq.com>
2020-11-23 00:24:42 +0800

    声明变量常量，并输出

--- src/FirstSample.java
+++ src/FirstSample.java
@@ -1,3 +1,5 @@
+import static java.lang.Math.*;
+
 public class FirstSample {
     public static void main(String[] args) {
         System.out.println("We will not use 'Hello World!'");
@@ -11,9 +13,25 @@ public class FirstSample {
         double paperHeight = 11;
         System.out.println("Paper size in centimeters:"
                 + paperWidth * CM_PER_INCH + "by" + paperHeight * CM_PER_INCH);
+        double x = 4;
+        double y = Math.sqrt(x);
+        System.out.println(y);
+        double a = 10;
+        double y1 = pow(x, a);
+        System.out.println(y1);
+        double d = 9.997;
+        int nd = (int) Math.round(d);
+        System.out.println(nd);
+        int m = 7;
+        int n = 7;
+        int e = 2 * ++m;
+        int f = 2 * n++;
+        System.out.println(e + "            " + f);
+        String greeting = "hello";
+        System.out.println("Hello".equals(greeting));
+        System.out.println("Hello".equalsIgnoreCase(greeting));
     }
 }
 /**
- *
  *
  */
