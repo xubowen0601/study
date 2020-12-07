0f1c6877a409687a42f8257ccc2b18b249d79ccf
xubowen0601 <786875770@qq.com>
2020-12-07 11:02:27 +0800

    尝试

--- src/Message.java
+++ src/Message.java
@@ -1,15 +1,13 @@
 public class Message {
     public static void main(String[] args) {
-        if (args.length==0||args[0].equals("-h"))
-        {
+        if (args.length == 0 || args[0].equals("-h")) {
             System.out.print("Hello,");
-        }else if (args[0].equals("-g")){
+        } else if (args[0].equals("-g")) {
             System.out.print("GoodBye,");
         }
         //print the other command-line arguments 打印其他命令行参数
-        for (int i=1;i< args.length;i++)
-        {
-            System.out.print(" "+args[i]);
+        for (int i = 1; i < args.length; i++) {
+            System.out.print(" " + args[i]);
         }
         System.out.println("!");
     }
