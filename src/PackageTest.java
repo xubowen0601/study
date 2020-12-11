3f275fbf6a7b744d1cb69db696dabe707c1ccfc1
xubowen0601 <786875770@qq.com>
2020-12-11 16:04:02 +0800

    测试使用包的效果

---
+++ src/PackageTest.java
@@ -0,0 +1,27 @@
+import com.horstmann.corejava.Employee;
+
+import static java.lang.System.*;
+//the Employee class is defined in that package
+//该包中定义了Employee类
+
+/**
+ * @author xubow
+ */
+public class PackageTest
+{
+    public static void main(String[] args)
+    {
+//        because of the import statement,we don't have to use
+//        由于导入语句，我们不必使用
+//        com.horstmann.corejava.Employee here
+//        com.horstmann.corejava.Employee在这里
+        Employee harry = new Employee("Harry Hacker",
+                50000, 1989, 10, 1);
+
+        harry.raiseSalary(5);
+
+//        because of the static import statement,we don't have to use System.out here
+        out.println("name=" + harry.getName()
+                + ",salary=" + harry.getSalary());
+    }
+}
---
+++ src/com/horstmann/corejava/Employee.java
@@ -0,0 +1,43 @@
+package com.horstmann.corejava;
+//the classes in this file are part of this package
+//该文件中的类是此软件包的一部分
+
+import java.time.*;
+
+/**
+ * @author xubow
+ */
+public class Employee
+{
+    private String name;
+    private double salary;
+    private LocalDate hireDay;
+
+    public Employee(String name, double salary, int year, int month, int day)
+    {
+        this.name = name;
+        this.salary = salary;
+        hireDay = LocalDate.of(year, month, day);
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public double getSalary()
+    {
+        return salary;
+    }
+
+    public LocalDate getHireDay()
+    {
+        return hireDay;
+    }
+
+    public void raiseSalary(double byPercent)
+    {
+        double raise = salary * byPercent / 100;
+        salary += raise;
+    }
+}
